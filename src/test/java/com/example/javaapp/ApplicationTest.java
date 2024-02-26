cab5315f1036f7164f67eb6790c4d207f033e6a4
Kastet6398 <konstantin.nezhalsky@gmail.com>
2024-02-26 10:57:19 +0200

    3

---
+++ src/test/java/com/example/javaapp/ApplicationTest.java
@@ -0,0 +1,12 @@
+package com.example.javaapp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ApplicationTest {
+    @Test
+    public void testSomething() {
+        System.out.println("Hello, test!");
+    }
+}
